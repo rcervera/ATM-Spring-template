f277204d319eb88349ad31e523dd6f3ca437f24a
Ramon Cervera <rcerver4@xtec.cat>
2024-01-15 14:19:20 +0100

    first commit

---
+++ src/main/java/com/daw/atm/controllers/ATMController.java
@@ -0,0 +1,14 @@
+package com.daw.atm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ATMController {
+     
+    @GetMapping("/")
+    public String inici() {
+        return "inici";
+    }
+}
