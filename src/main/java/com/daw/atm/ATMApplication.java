8905aee5be4881e8f95c99d2d0a3c7b01bd281e6
Ramon Cervera <rcerver4@xtec.cat>
2024-01-17 18:02:04 +0100

    first commit

--- src/main/java/com/daw/atm/ATMApplication.java
+++ src/main/java/com/daw/atm/ATMApplication.java
@@ -3,8 +3,6 @@ package com.daw.atm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.daw.atm.model.Person;
-
 
 @SpringBootApplication
 public class ATMApplication {
